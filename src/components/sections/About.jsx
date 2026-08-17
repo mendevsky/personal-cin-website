aed847f81425b17291f80895ed64b09701934b6a
Iasmym Mendes <ilmf@cin.ufpe.br>
2026-08-16 22:55:10 -0300

    Made styling changes to better fit the color palette and overall aesthetic

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -10,14 +10,16 @@ import { GiBookCover } from "react-icons/gi";
 import { FaGithub } from "react-icons/fa";
 import { FaJava } from "react-icons/fa";
 
+
 function About() {
     return (
-        <div className="about grid grid-cols-2" id="about">
-            <div className="flex justify-center items-center pt-15 flex-col px-4">
-                <p className="text-7xl">About Me</p>
 
-                <div className="rounded-md bg-gray-200 p-4 mt-15">
-                    <p className="text-xl text-gray-800">
+        <section className="about grid grid-cols-2" id="about">
+            <div className="flex justify-center items-center pt-15 flex-col px-4">
+                <p className="text-7xl font-bold mb-10 bg-gradient-to-r from-red-900 to-red-900 bg-clip-text text-transparent">About Me</p>
+
+                <div className="p-6 rounded-xl border border-gray-200 shadow-md hover:translate-y-1 hover:border-red-500 hover:shadow-[0_2px_08px_rgba(255,0,0,0.3)] transition-all">
+                    <p className="text-gray-700 mb-4">
                         My journey with CS at CIn-UFPE begun long time ago, starting with a math teacher in high school that introduced me to this amazing place.
                         Although I still don't have many interesting stories to share, I believe that my personal journey with Autism has shaped my understanting of the world and my approach to problem-solving. I am passionate about exploring the intersection of technology and neurodiversity, and I am eager to contribute to projects that promote inclusivity and accessibility in the digital space.
                         My personal interests are diverse, from programming languages and IT support to tabletop RPGs and video games. I love technology, science, astronomy and pop culture, and I am always seeking new ways to expand my knowledge and skills. I am excited to continue my journey in the field of CS and to make a positive impact in the world through my work. 
@@ -28,45 +30,46 @@ function About() {
 
             <div className="flex justify-center items-center pt-10 flex-col px-4">
 
-                <p className="text-7xl">My Interests</p>
+                <p className="text-7xl font-bold mb-10 bg-gradient-to-r from-red-900 to-red-900 bg-clip-text text-transparent">
+                    My Interests</p>
                   <div className="flex justify-center items-center mt-10">
                     <a href="https://www.python.org/" target="_blank" rel="noopener noreferrer" className="animate-bounce">
-                        <FaPython size={50} className="text-gray-800 hover:text-gray-600 transition-colors duration-300" />
+                        <FaPython size={50} className="text-red-900 hover:text-red-600 transition-colors duration-300" />
                     </a>
                     <a href="https://www.linux.org/" target="_blank" rel="noopener noreferrer" className="animate-bounce ml-4">
-                        <FaLinux size={50} className="text-gray-800 hover:text-gray-600 transition-colors duration-300" />
+                        <FaLinux size={50} className="text-red-900 hover:text-red-600 transition-colors duration-300" />
                     </a>
                     <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer" className="animate-bounce ml-4">
-                        <RiReactjsFill size={50} className="text-gray-800 hover:text-gray-600 transition-colors duration-300" />
+                        <RiReactjsFill size={50} className="text-red-900 hover:text-red-600 transition-colors duration-300" />
                     </a>
                     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noopener noreferrer" className="animate-bounce ml-4">
-                        <FaJsSquare size={50} className="text-gray-800 hover:text-gray-600 transition-colors duration-300" />
+                        <FaJsSquare size={50} className="text-red-900 hover:text-red-600 transition-colors duration-300" />
                     </a>
                     <a href="https://github.com/" target="_blank" rel="noopener noreferrer" className="animate-bounce ml-4">
-                        <FaGithub size={50} className="text-gray-800 hover:text-gray-600 transition-colors duration-300" />
+                        <FaGithub size={50} className="text-red-900 hover:text-red-600 transition-colors duration-300" />
                     </a>
                     <a href="https://www.java.com/" target="_blank" rel="noopener noreferrer" className="animate-bounce ml-4">
-                        <FaJava size={50} className="text-gray-800 hover:text-gray-600 transition-colors duration-300" />
+                        <FaJava size={50} className="text-red-900 hover:text-red-600 transition-colors duration-300" />
                     </a>
 
                   </div>
 
                   <div className="flex justify-center items-center mt-10">
                     <a href="https://www.critrole.com/" target="_blank" rel="noopener noreferrer" className="animate-bounce">
-                        <FaCriticalRole size={50} className="text-gray-800 hover:text-gray-600 transition-colors duration-300" />
+                        <FaCriticalRole size={50} className="text-red-900 hover:text-red-600 transition-colors duration-300" />
                     </a>
 
                     <a href="https://dungeonsanddragons.com/" target="_blank" rel="noopener noreferrer" className="animate-bounce ml-4">
-                        <SiDungeonsanddragons size={50} className="text-gray-800 hover:text-gray-600 transition-colors duration-300" />
+                        <SiDungeonsanddragons size={50} className="text-red-900 hover:text-red-600 transition-colors duration-300" />
                     </a>
                     <a href="https://store.steampowered.com/" target="_blank" rel="noopener noreferrer" className="animate-bounce ml-4">
-                        <FaSteam size={50} className="text-gray-800 hover:text-gray-600 transition-colors duration-300" />
+                        <FaSteam size={50} className="text-red-900 hover:text-red-600 transition-colors duration-300" />
                     </a>
                     <a href="https://www.nasa.gov/" target="_blank" rel="noopener noreferrer" className="animate-bounce ml-4">
-                        <SiNasa size={50} className="text-gray-800 hover:text-gray-600 transition-colors duration-300" />
+                        <SiNasa size={50} className="text-red-900 hover:text-red-600 transition-colors duration-300" />
                     </a>
                     <a href="https://www.goodreads.com/" target="_blank" rel="noopener noreferrer" className="animate-bounce ml-4">
-                        <GiBookCover size={50} className="text-gray-800 hover:text-gray-600 transition-colors duration-300" />
+                        <GiBookCover size={50} className="text-red-900 hover:text-red-600 transition-colors duration-300" />
                     </a>
 
                   </div>
@@ -75,7 +78,7 @@ function About() {
 
             </div>
 
-        </div>
+        </section>
 
 
     )
--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -11,10 +11,11 @@ function Home (){
 
         <section className="home grid grid-cols-2" id="home" >
               <div className="flex justify-center items-center pt-35 flex-col px-4"> {/* Left side of the home section, where the student will be introduced */}
-                <p className="text-7xl">Hello, world!</p>
+                <p className="text-7xl font-bold mb-10 bg-gradient-to-r from-red-900 to-red-900 bg-clip-text text-transparent">
+                    Hello, world!</p>
                 
-                <div className="rounded-md bg-gray-200 p-4 mt-15">
-                    <p className="text-xl text-gray-800">
+                <div className="p-6 rounded-xl border border-gray-200 shadow-md hover:translate-y-1 hover:border-red-500 hover:shadow-[0_2px_08px_rgba(255,0,0,0.3)] transition-all">
+                    <p className="text-gray-700 mb-4">
                         My name is Iasmym Mendes and I am a student at Universidade Federal de Pernambuco, currently pursuing a degree in Computer Science. I have a strong passion for technology and enjoy exploring new programming languages and frameworks. In my free time, I like to work on personal projects, study topics in Neurodiversity and explore the world of pop culture.
                     </p>
                 </div>
@@ -24,20 +25,20 @@ function Home (){
                 <img src={"src/assets/iasFace.png"} alt="Iasmym Mendes" className="h-120 w-auto border-4 border-white rounded-full" />
                 <div className="flex justify-center items-center mt-4">
                     <a href="https://github.com/mendevsky" target="_blank" rel="noopener noreferrer">
-                        <FaGithub size={50} className="text-gray-800 hover:text-gray-600 hover:animate-bounce transition-colors duration-300" />
+                        <FaGithub size={50} className="text-red-900 hover:text-red-600 hover:animate-bounce transition-colors duration-300" />
                     </a>
                     <a href="https://www.linkedin.com/in/iaslmf/" target="_blank" rel="noopener noreferrer">
-                        <FaLinkedin size={50} className="text-gray-800 hover:text-gray-600 hover:animate-bounce transition-colors duration-300 ml-4" />
+                        <FaLinkedin size={50} className="text-red-900 hover:text-red-600 hover:animate-bounce transition-colors duration-300 ml-4" />
                     </a>
                     <a href="mailto:ilmf@cin.ufpe.br" target="_blank" rel="noopener noreferrer">
-                        <MdOutlineAlternateEmail size={50} className="text-gray-800 hover:text-gray-600 hover:animate-bounce transition-colors duration-300 ml-4" />
+                        <MdOutlineAlternateEmail size={50} className="text-red-900 hover:text-red-600 hover:animate-bounce transition-colors duration-300 ml-4" />
                     </a>
                     
                     <a href="https://www.instagram.com/iaslmf/" target="_blank" rel="noopener noreferrer"    >
-                        <FaInstagram size={50} className="text-gray-800 hover:text-gray-600 hover:animate-bounce transition-colors duration-300 ml-4" />
+                        <FaInstagram size={50} className="text-red-900 hover:text-red-600 hover:animate-bounce transition-colors duration-300 ml-4" />
                     </a>
                     <a href="https://twitter.com/_iaslmf" target="_blank" rel="noopener noreferrer">
-                        <FaXTwitter size={50} className="text-gray-800 hover:text-gray-600 hover:animate-bounce transition-colors duration-300 ml-4" />
+                        <FaXTwitter size={50} className="text-red-900 hover:text-red-600 hover:animate-bounce transition-colors duration-300 ml-4" />
                     </a>
 
 
